cfda1886dccec82b1973da75aabd136699018f0f
delano campos <delanoacampos@gmail.com>
2026-03-17 16:57:17 -0500

    new run files

--- run_reduced.py
+++ run_reduced.py
@@ -7,7 +7,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Phase2C17I13M9_cff import Phase2C17I13M9
 
-process = cms.Process('L1P2GT',Phase2C17I13M9)
+process = cms.Process('L1P2GT2',Phase2C17I13M9)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -17,11 +17,12 @@ process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 process.load('Configuration.Geometry.GeometryExtendedRun4D121Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load('Configuration.StandardSequences.SimL1Emulator_cff')
 process.load('Configuration.StandardSequences.SimPhase2L1GlobalTriggerEmulator_cff')
 process.load('L1Trigger.Configuration.Phase2GTMenus.SeedDefinitions.step1_2024.l1tGTMenu_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('L1Trigger.Phase2L1GMT.gmtKMTFMuons_cfi')
+process.load('L1Trigger.Phase2L1GMT.gmtStubs_cfi')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(50),
@@ -31,11 +32,9 @@ process.maxEvents = cms.untracked.PSet(
 # Input source
 process.source = cms.Source("PoolSource",
     dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
-    fileNames = cms.untracked.vstring('/store/mc/Phase2Spring24DIGIRECOMiniAOD/DYToLL_M-50_TuneCP5_14TeV-pythia8/GEN-SIM-DIGI-RAW-MINIAOD/PU200_Trk1GeV_140X_mcRun4_realistic_v4-v1/2810000/5bed6cad-6cdb-4a5e-87af-08cc8c8e0ff8.root'),
+    fileNames = cms.untracked.vstring('file:output_Phase2_L1T.root'),
     inputCommands = cms.untracked.vstring(
-        'keep *',
-        'drop l1tPFJets_*_*_*',
-        'drop l1tTrackerMuons_l1tTkMuonsGmt*_*_HLT'
+        'keep *'
     ),
     secondaryFileNames = cms.untracked.vstring()
 )
@@ -80,18 +79,31 @@ process.configurationMetadata = cms.untracked.PSet(
 )
 
 # Output definition
+process.FEVTDEBUGHLTEventContent.outputCommands=cms.untracked.vstring(
+    'drop *',
+    'keep *_simDtTriggerPrimitiveDigis_*_*',
+    "keep *_dtTriggerPhase2PrimitivePairDigis_*_*",
+    'keep *_genParticles_*_*',
+    'keep *_dtTriggerPhase2PrimitiveDigis_*_*',
+    "keep *_simMuonDTDigis_*_*",
+    "keep *_gmtStubs_*_*",
+    "keep *_gmtKMTFMuons_*_*",
+    "keep *_l1tSAMuonsGmt_*_*",
+    "keep *_l1tKMTFMuonsGmt_*_*",
+    "keep *_l1tStubsGmt_*_*",
+    "keep *_simBmtfDigis_*_*",
+    "keep *_dtTriggerPhase2PrimitivePairDigis_*_*",
+    "keep *_simCscTriggerPrimitiveDigis_*_*",
+    "keep *_simMuonRPCDigis_*_*")
 process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
     dataset = cms.untracked.PSet(
         dataTier = cms.untracked.string('GEN-SIM-DIGI-RAW-MINIAOD'),
         filterName = cms.untracked.string('')
     ),
-    fileName = cms.untracked.string('file:output_Phase2_L1T.root'),
+    fileName = cms.untracked.string('file:output_DY_Phase2_L1T_KMTF.root'),
     outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
     splitLevel = cms.untracked.int32(0)
 )
-
-
-
 
 
 # Additional output definition
@@ -101,55 +113,13 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T35', '')
 
 # Path and EndPath definitions
-process.L1simulation_step = cms.Path(process.SimL1Emulator)
-process.Phase2L1GTProducer = cms.Path(process.l1tGTProducerSequence)
-process.Phase2L1GTAlgoBlockProducer = cms.Path(process.l1tGTAlgoBlockProducerSequence)
-process.pDoubleEGEle37_24 = cms.Path(process.DoubleEGEle3724)
-process.pDoubleIsoTkPho22_12 = cms.Path(process.DoubleIsoTkPho2212)
-process.pDoublePuppiJet112_112 = cms.Path(process.DoublePuppiJet112112)
-process.pDoublePuppiJet160_35_mass620 = cms.Path(process.DoublePuppiJet16035Mass620)
-process.pDoublePuppiTau52_52 = cms.Path(process.DoublePuppiTau5252)
-process.pDoubleTkEle25_12 = cms.Path(process.DoubleTkEle2512)
-process.pDoubleTkElePuppiHT_8_8_390 = cms.Path(process.DoubleTkElePuppiHT)
-process.pDoubleTkMuPuppiHT_3_3_300 = cms.Path(process.DoubleTkMuPuppiHT)
-process.pDoubleTkMuPuppiJetPuppiMet_3_3_60_130 = cms.Path(process.DoubleTkMuPuppiJetPuppiMet)
-process.pDoubleTkMuon15_7 = cms.Path(process.DoubleTkMuon157)
-process.pDoubleTkMuonTkEle5_5_9 = cms.Path(process.DoubleTkMuonTkEle559)
-process.pDoubleTkMuon_4_4_OS_Dr1p2 = cms.Path(process.DoubleTkMuon44OSDr1p2)
-process.pDoubleTkMuon_4p5_4p5_OS_Er2_Mass7to18 = cms.Path(process.DoubleTkMuon4p5OSEr2Mass7to18)
-process.pDoubleTkMuon_OS_Er1p5_Dr1p4 = cms.Path(process.DoubleTkMuonOSEr1p5Dr1p4)
-process.pIsoTkEleEGEle22_12 = cms.Path(process.IsoTkEleEGEle2212)
-process.pNNPuppiTauPuppiMet_55_190 = cms.Path(process.NNPuppiTauPuppiMet)
-process.pPuppiHT400 = cms.Path(process.PuppiHT400)
-process.pPuppiHT450 = cms.Path(process.PuppiHT450)
-process.pPuppiMET200 = cms.Path(process.PuppiMET200)
-process.pPuppiMHT140 = cms.Path(process.PuppiMHT140)
-process.pPuppiTauTkIsoEle45_22 = cms.Path(process.PuppiTauTkIsoEle4522)
-process.pPuppiTauTkMuon42_18 = cms.Path(process.PuppiTauTkMuon4218)
-process.pQuadJet70_55_40_40 = cms.Path(process.QuadJet70554040)
-process.pSingleEGEle51 = cms.Path(process.SingleEGEle51)
-process.pSingleIsoTkEle28 = cms.Path(process.SingleIsoTkEle28)
-process.pSingleIsoTkPho36 = cms.Path(process.SingleIsoTkPho36)
-process.pSinglePuppiJet230 = cms.Path(process.SinglePuppiJet230)
-process.pSingleTkEle36 = cms.Path(process.SingleTkEle36)
-process.pSingleTkMuon22 = cms.Path(process.SingleTkMuon22)
-process.pTkEleIsoPuppiHT_26_190 = cms.Path(process.TkEleIsoPuppiHT)
-process.pTkElePuppiJet_28_40_MinDR = cms.Path(process.TkElePuppiJetMinDR)
-process.pTkEleTkMuon10_20 = cms.Path(process.TkEleTkMuon1020)
-process.pTkMuPuppiJetPuppiMet_3_110_120 = cms.Path(process.TkMuPuppiJetPuppiMet)
-process.pTkMuTriPuppiJet_12_40_dRMax_DoubleJet_dEtaMax = cms.Path(process.TkMuTriPuppiJetdRMaxDoubleJetdEtaMax)
-process.pTkMuonDoubleTkEle6_17_17 = cms.Path(process.TkMuonDoubleTkEle61717)
-process.pTkMuonPuppiHT6_320 = cms.Path(process.TkMuonPuppiHT6320)
-process.pTkMuonTkEle7_23 = cms.Path(process.TkMuonTkEle723)
-process.pTkMuonTkIsoEle7_20 = cms.Path(process.TkMuonTkIsoEle720)
-process.pTripleTkMuon5_3_3 = cms.Path(process.TripleTkMuon533)
-process.pTripleTkMuon_5_3_0_DoubleTkMuon_5_3_OS_MassTo9 = cms.Path(process.TripleTkMuon530OSMassMax9)
-process.pTripleTkMuon_5_3p5_2p5_OS_Mass5to17 = cms.Path(process.TripleTkMuon53p52p5OSMass5to17)
+process.GMTStubProducer_offline = cms.Path(process.gmtStubs)
+process.GMTKMTFMuons_offline = cms.Path(process.gmtKMTFMuons)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.L1simulation_step,process.Phase2L1GTProducer,process.Phase2L1GTAlgoBlockProducer,process.pDoubleEGEle37_24,process.pDoubleIsoTkPho22_12,process.pDoublePuppiJet112_112,process.pDoublePuppiJet160_35_mass620,process.pDoublePuppiTau52_52,process.pDoubleTkEle25_12,process.pDoubleTkElePuppiHT_8_8_390,process.pDoubleTkMuPuppiHT_3_3_300,process.pDoubleTkMuPuppiJetPuppiMet_3_3_60_130,process.pDoubleTkMuon15_7,process.pDoubleTkMuonTkEle5_5_9,process.pDoubleTkMuon_4_4_OS_Dr1p2,process.pDoubleTkMuon_4p5_4p5_OS_Er2_Mass7to18,process.pDoubleTkMuon_OS_Er1p5_Dr1p4,process.pIsoTkEleEGEle22_12,process.pNNPuppiTauPuppiMet_55_190,process.pPuppiHT400,process.pPuppiHT450,process.pPuppiMET200,process.pPuppiMHT140,process.pPuppiTauTkIsoEle45_22,process.pPuppiTauTkMuon42_18,process.pQuadJet70_55_40_40,process.pSingleEGEle51,process.pSingleIsoTkEle28,process.pSingleIsoTkPho36,process.pSinglePuppiJet230,process.pSingleTkEle36,process.pSingleTkMuon22,process.pTkEleIsoPuppiHT_26_190,process.pTkElePuppiJet_28_40_MinDR,process.pTkEleTkMuon10_20,process.pTkMuPuppiJetPuppiMet_3_110_120,process.pTkMuTriPuppiJet_12_40_dRMax_DoubleJet_dEtaMax,process.pTkMuonDoubleTkEle6_17_17,process.pTkMuonPuppiHT6_320,process.pTkMuonTkEle7_23,process.pTkMuonTkIsoEle7_20,process.pTripleTkMuon5_3_3,process.pTripleTkMuon_5_3_0_DoubleTkMuon_5_3_OS_MassTo9,process.pTripleTkMuon_5_3p5_2p5_OS_Mass5to17,process.endjob_step,process.FEVTDEBUGHLToutput_step)
+process.schedule = cms.Schedule(process.GMTStubProducer_offline, process.GMTKMTFMuons_offline, process.endjob_step, process.FEVTDEBUGHLToutput_step)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
 
